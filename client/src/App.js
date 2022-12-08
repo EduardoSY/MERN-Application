95b16ead61f538467f7ee44be356f4d4136e2eb1
Eduardo Da Silva <alu0101104911@ull.edu.es>
2022-12-08 13:20:15 +0000

    Creado ReactAPP + SASS + SemanticUI

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>
+        Estamos en mi web
+      </h1>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
+    </div>
+  )
+}
